1f928d50108e8f8708ccb9299294f5593a232d93
Stevie Builds <steve.richardson.dev@gmail.com>
2026-03-04 05:22:45 +0000

    hotfix: filter date-like tokens from legacy phone extraction

--- src/lib/legacy/lead-normalization.ts
+++ src/lib/legacy/lead-normalization.ts
@@ -98,7 +98,7 @@ function normalizePhones(doc: AnyDoc) {
     doc.contactPhone,
     doc.primaryPhone,
     ...fromNotes,
-  ]);
+  ]).filter((value) => !/^\d{4}-\d{2}-\d{2}$/.test(value));
 }
 
 function normalizeStatus(value: string) {
